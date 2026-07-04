b1b439cc4b30aabe5c277462cacbabca326d56ba
Abhishek Malakar <abhishekmalakar631@gmail.com>
2026-07-04 10:47:55 +0530

    Created required files and folders

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask ,render_template,request,redirect,url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "App Run"
+
+if __name__=="__main__":
+    app.run(debug=True)
